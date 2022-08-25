b649edf3793caba4aa552ee07ce60034904bb013
Ace0012 <ayushchandra042003@gmail.com>
2022-08-25 23:29:20 +0530

    done

--- niftypm/src/App.js
+++ niftypm/src/App.js
@@ -2,13 +2,16 @@
 // import Grids from './components/Navbar/NAN';
 // import Grid from './components/Navbar/NAN';
 // import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Navbar/Footer';
+// import Footer from './components/Navbar/Footer';
+// import Footer from './components/footer/Footer';
+import Homepage from './components/Home/Homepage';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
-      <Footer/>
+      {/* <Footer/> */}
+      <Homepage/>
       
 
 
